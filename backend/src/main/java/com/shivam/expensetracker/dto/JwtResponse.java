8dd20603943e6fcc6e3ff85915138a955c9c55f6
SHIVAM KUMAR <shivamkumarhub@gmail.com>
2026-09-07 20:24:44 +0530

    feat: JwtResponse DTO

---
+++ backend/src/main/java/com/shivam/expensetracker/dto/JwtResponse.java
@@ -0,0 +1,19 @@
+package com.shivam.expensetracker.dto;
+
+import java.util.Set;
+
+public class JwtResponse {
+    private String token;
+    private String type;
+    private String username;
+    private Set<String> roles;
+
+    public JwtResponse(String token, String type, String username, Set<String> roles) {
+        this.token = token; this.type = type; this.username = username; this.roles = roles;
+    }
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+    public String getType() { return type; }
+    public String getUsername() { return username; }
+    public Set<String> getRoles() { return roles; }
+}
