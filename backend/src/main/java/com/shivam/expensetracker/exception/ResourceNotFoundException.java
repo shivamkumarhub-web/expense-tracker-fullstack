cdc324657aa638cdd4dec5ff129b6f5e569bacca
SHIVAM KUMAR <shivamkumarhub@gmail.com>
2026-09-07 20:25:50 +0530

    feat: ResourceNotFoundException

---
+++ backend/src/main/java/com/shivam/expensetracker/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.shivam.expensetracker.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) { super(message); }
+}
